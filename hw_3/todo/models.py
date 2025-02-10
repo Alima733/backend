bfe3e5ba741eac333e5cf297e3cd783b1a6eae84
Alima <alzhh073@gmail.com>
2025-02-10 20:17:47 +0500

    Added Todo CRUD functionality

---
+++ hw_3/todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
+# Create your models here.
